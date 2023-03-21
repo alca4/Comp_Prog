a485bbd0b1bbe11b5c05828f96a9dc3247132d62
alca4 <alcago2020@gmail.com>
2023-03-21 09:21:57 -0700

    03_21_2023

---
+++ AtCoder/ARC/ARC154_D_A+B>C.cpp
@@ -0,0 +1,148 @@
+/*
+  ___
+ (o,o)
+<  .  >
+--"-"---
+Rowlet is orz
+  _      _      _
+>(.)__ <(.)__ =(.)__
+ (___/  (___/  (___/
+I am dum duck
+
+Praise to the Cow God
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FF first
+#define SS second
+#define pb push_back
+#define ll long long
+#define ld long double
+#define ull unsigned ll
+#define endl "\n"
+ll INF = INT_MAX;
+ll LINF = LONG_MAX;
+ll MOD = 1000000007;
+// ll MOD = 998244353;
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+ll add(const ll& a, const ll& b) 
+{
+    ll x = a + b;
+    if (a + b >= MOD) x -= MOD;
+    return x;
+}
+ll sub(const ll& a, const ll& b) 
+{
+    ll x = a - b;
+    if (a - b < 0) x += MOD;
+    return x;
+}
+ll mult(const ll& a, const ll& b) {return (a * b) % MOD;}
+ll power(ll a, ll b)
+{
+    ll n = b;
+    ll ans = 1;
+
+    ll binexp[64];
+    binexp[0] = a;
+    for (int i = 1; i < 64; i++) binexp[i] = mult(binexp[i - 1], binexp[i - 1]);
+
+    while (n > 0)
+    {
+        int id = (int) log2(n & -n);
+        ans = mult(ans, binexp[id]);
+        n -= (n & -n);
+    }
+
+    return ans;
+}
+ll divide(const ll& a, const ll& b) {return (a * power(b, MOD - 2)) % MOD;}
+template<class X, class Y> void maxeq(X &x, Y y) {if (x < y) x = y;}
+template<class X, class Y> void mineq(X &x, Y y) {if (x > y) x = y;}
+template<class X, class Y> void addeq(X &x, Y y) {x = add(x, y);}
+template<class X, class Y> void subeq(X &x, Y y) {x = sub(x, y);}
+template<class X, class Y> void multeq(X &x, Y y) {x = mult(x, y);}
+template<class X, class Y> void diveq(X &x, Y y) {x = divide(x, y);}
+
+int rand32()
+{
+    int a = rand();
+    int b = rand();
+    return (a << 16) | b;
+}
+
+ll rand64()
+{
+    ll a = rand32();
+    ll b = rand32();
+    return (a << 32) | b;
+}
+
+const int MAXN = 2010;
+int N;
+int ans[MAXN];
+ll fact[MAXN], factinv[MAXN];
+
+ll choose(int a, int b)
+{
+    return mult(fact[a], mult(factinv[b], factinv[a - b]));
+}
+
+void get_fact(int x)
+{
+    fact[0] = 1;
+    for (int i = 1; i <= x; i++) fact[i] = mult(i, fact[i - 1]);
+    factinv[x] = divide(1, fact[x]);
+    for (int i = x - 1; i >= 0; i--) factinv[i] = mult(factinv[i + 1], i + 1);
+}
+
+int one = 1;
+
+bool check(int a, int b, int c)
+{
+    cout << "? " << a << " " << b << " " << c << endl;
+    cout.flush();
+    string str;
+    cin >> str;
+
+    if (str == "Yes") return 1;
+    else return 0;
+}
+
+bool Comp(int a, int b)
+{
+    return !check(a, one, b);
+}
+
+int main()
+{
+    // freopen('.in', 'r', cin);
+    // freopen('.out', 'w', cout);
+    srand(time(NULL));
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> N;
+
+    for (int i = 2; i <= N; i++)
+        if (!check(i, i, one)) one = i;
+    
+    vector<int> ids;
+    for (int i = 1; i <= N; i++) if (i != one) ids.pb(i);
+    stable_sort(ids.begin(), ids.end(), Comp);
+
+    for (int i = 0; i < ids.size(); i++) ans[ids[i]] = i + 2;
+    ans[one] = 1;
+
+    cout << "! ";
+    for (int i = 1; i <= N; i++) cout << ans[i] << " ";
+    cout << endl;
+    cout.flush();
+
+    return 0;
+} 
